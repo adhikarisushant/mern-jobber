3bcba1b1b4bd74b4f5331d1be6107be70c9ea714
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-04-04 16:10:28 +0545

    Get all Jobs Controller

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -15,7 +15,11 @@ const createJob = async (req, res) => {
 }
 
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs')
+    const jobs = await Job.find({ createdBy: req.user.userId })
+
+    res
+        .status(StatusCodes.OK)
+        .json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
 }
 
 const updateJob = async (req, res) => {
