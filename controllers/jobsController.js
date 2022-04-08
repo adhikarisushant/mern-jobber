65a0e5262ee1c8763475b33d8e13d6bb30a2598d
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-04-08 21:33:32 +0545

    stats

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -2,6 +2,7 @@ import Job from '../models/Job.js'
 import {StatusCodes} from 'http-status-codes'
 import { BadRequestError, NotFoundError, UnAuthenticatedError } from '../errors/index.js'
 import checkPermissions from '../utils/CheckPermissions.js'
+import mongoose from 'mongoose'
 
 const createJob = async (req, res) => {
     const {position, company} = req.body
@@ -66,7 +67,26 @@ const deleteJob = async (req, res) => {
 }
 
 const showStats = async (req, res) => {
-    res.send('show stats')
+    let stats = await Job.aggregate([
+        {$match:{createdBy:mongoose.Types.ObjectId(req.user.userId)}},
+        {$group:{ _id:'$status',count:{ $sum:1 } }}
+    ])
+
+    stats = stats.reduce((acc, curr) => {
+        const { _id: title, count } = curr
+        acc[title] = count
+        return acc
+    }, {})
+
+    const defaultStats = {
+        pending: stats.pending || 0,
+        interview: stats.interview || 0,
+        declined: stats.declined || 0,
+    }
+    
+    let monthlyApplications = []
+
+    res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
 }
 
 
