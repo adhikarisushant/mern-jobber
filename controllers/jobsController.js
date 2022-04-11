92fb53f018420b4675bf765fcf52b2d1bd4a78ad
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-04-11 16:52:48 +0545

    monthly applications

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -3,6 +3,7 @@ import {StatusCodes} from 'http-status-codes'
 import { BadRequestError, NotFoundError, UnAuthenticatedError } from '../errors/index.js'
 import checkPermissions from '../utils/CheckPermissions.js'
 import mongoose from 'mongoose'
+import moment from 'moment'
 
 const createJob = async (req, res) => {
     const {position, company} = req.body
@@ -82,7 +83,42 @@ const showStats = async (req, res) => {
       interview: stats.interview || 0,
       declined: stats.declined || 0,
     }
-    let monthlyApplications = []
+
+
+    let monthlyApplications = await Job.aggregate([
+      { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+      {
+        $group: {
+          _id: {
+            year: {
+              $year: '$createdAt',
+            },
+            month: {
+              $month: '$createdAt',
+            },
+          },
+          count: { $sum: 1 },
+        },
+      },
+      { $sort: { '_id.year': -1, '_id.month': -1 } },
+      { $limit: 6 },
+    ])
+
+    monthlyApplications = monthlyApplications
+    .map((item) => {
+      const {
+        _id: { year, month },
+        count,
+      } = item
+      // accepts 0-11
+      const date = moment()
+        .month(month - 1)
+        .year(year)
+        .format('MMM Y')
+      return { date, count }
+    })
+    .reverse()
+
     res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications })
   }
 
