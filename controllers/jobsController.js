d36f2e8cd8f5cd05bc5193443613aad1d3c9e6d3
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-04-15 15:40:44 +0545

    pagination

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -54,11 +54,19 @@ const getAllJobs = async (req, res) => {
       result = result.sort('-position')
     }
     
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit //10
+    result = result.skip(skip).limit(limit)
+
     const jobs = await result
+
+    const totalJobs = await Job.countDocuments(queryObject)
+    const numOfPages = Math.ceil(totalJobs / limit)
 
     res
         .status(StatusCodes.OK)
-        .json({ jobs, totalJobs: jobs.length, numOfPages: 1 })
+        .json({ jobs, totalJobs, numOfPages })
 }
 
 const updateJob = async (req, res) => {
