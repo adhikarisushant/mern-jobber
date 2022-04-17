9bdf892b1a53afb2b8c8cd0eb613a76e2493f633
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-04-17 12:47:12 +0545

    Ready for Deployment

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 import express from 'express'
-
 const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 import 'express-async-errors'
 import morgan from 'morgan'
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import mongoSanitize from 'express-mongo-sanitize'
 
 // db and authenticateUser
 import connectDB from './db/connect.js'
@@ -22,7 +29,15 @@ if(process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'))
 }
 
+const __dirname = dirname(fileURLToPath(import.meta.url))
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname, './client/build')))
+
 app.use(express.json())
+app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
 
 app.get('/', (req, res) => {
     res.json({ msg: 'Welcome' })
@@ -30,6 +45,11 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+// only when ready to deploy
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, './client/build', 'index.html'))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
