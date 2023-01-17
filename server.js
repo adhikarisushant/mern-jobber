ebbf4b07b3eaee3021f4f5b292dfadc6abb6edec
adhikarisushant <sushant.adhikari.619@gmail.com>
2023-01-17 23:43:11 +0545

    ready to deploy

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const start = async () => {
             console.log(`Server is listening on port ${port}...`)
         })
     } catch (error) {
-
+        console.log(`Server responded with error => ${error}`)
     }
 }
 
