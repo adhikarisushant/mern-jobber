dfac3980ad78bc7443fce9a8e191f47bfcd2c03c
adhikarisushant <sushant.adhikari.619@gmail.com>
2023-01-20 21:37:49 +0545

    ready to deploy

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
+app.use(cors({
+    origin: '*'
+}));
 import dotenv from 'dotenv'
 dotenv.config()
 import 'express-async-errors'
